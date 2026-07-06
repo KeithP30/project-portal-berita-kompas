50f6cad72eef893faebfe67d9ca1f1655f57a0c0
KeithPx <kpgw30@gmail.com>
2026-07-06 12:59:16 +0700

    Fix failure tests 2

--- pages/article_page.py
+++ pages/article_page.py
@@ -6,7 +6,7 @@ class ArticlePage(BasePage):
     """Page Object untuk halaman detail artikel Kompas.com."""
     TITLE = (By.CSS_SELECTOR, "h1.read_title, h1")
     AUTHOR = (By.CSS_SELECTOR, ".credit-title-name")
-    DATE = (By.CSS_SELECTOR, ".read_date")
+    DATE = (By.CSS_SELECTOR, ".read_date, .read__time, time")
 
     HOME_BUTTON = (By.CSS_SELECTOR, "a[href='https://www.kompas.com']")
 
